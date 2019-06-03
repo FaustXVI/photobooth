fcabcc521d6305dc4e971e2eeaf5cc4632ee03b5
FaustXVI <xavier.detant@gmail.com>
2019-06-03 19:24:22 +0200

    Making it testable on a normal computer

--- button.py
+++ button.py
@@ -1,22 +1,37 @@
-import RPi.GPIO as GPIO
+try:
+    import RPi.GPIO as GPIO
 
 
-class Button:
-    def __init__(self, pin_number: int, action):
-        self.pin_number = pin_number
-        self.action = action
-        GPIO.setmode(GPIO.BOARD)
-        GPIO.setup(pin_number, GPIO.IN)
+    class Button:
+        def __init__(self, pin_number: int, action):
+            self.pin_number = pin_number
+            self.action = action
+            GPIO.setmode(GPIO.BOARD)
+            GPIO.setup(pin_number, GPIO.IN)
 
-    def __enter__(self):
-        return self
+        def __enter__(self):
+            return self
 
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        GPIO.cleanup()
+        def __exit__(self, exc_type, exc_val, exc_tb):
+            GPIO.cleanup()
 
-    def next_action(self):
-        input_state = GPIO.input(self.pin_number)
-        if input_state:
-            return self.action
-        else:
+        def next_action(self):
+            input_state = GPIO.input(self.pin_number)
+            if input_state:
+                return self.action
+            else:
+                return None
+
+except ImportError:
+    class Button:
+        def __init__(self, pin_number: int, action):
+            pass
+
+        def __enter__(self):
+            return self
+
+        def __exit__(self, exc_type, exc_val, exc_tb):
+            pass
+
+        def next_action(self):
             return None
--- camera.py
+++ camera.py
@@ -1,41 +1,60 @@
-from datetime import datetime
-import picamera
-import os
+try:
+    from datetime import datetime
+    import picamera
+    import os
 
-HD_RESOLUTION = (1920, 1080)
+    HD_RESOLUTION = (1920, 1080)
 
-IMAGE_WIDTH = 550
-IMAGE_HEIGHT = 360
+    IMAGE_WIDTH = 550
+    IMAGE_HEIGHT = 360
 
 
-class Camera:
-    def __init__(self, pictures_folder):
-        self.pictures_folder = pictures_folder
-        self.camera = picamera.PiCamera(resolution=HD_RESOLUTION,
-                                        framerate=30,
-                                        sensor_mode=5)
-        self.camera.rotation = 0
-        self.camera.hflip = True
-        self.camera.vflip = False
-        self.camera.brightness = 50
-        self.camera.preview_alpha = 120
-        self.camera.preview_fullscreen = True
+    class Camera:
+        def __init__(self, pictures_folder):
+            self.pictures_folder = pictures_folder
+            self.camera = picamera.PiCamera(resolution=HD_RESOLUTION,
+                                            framerate=30,
+                                            sensor_mode=5)
+            self.camera.rotation = 0
+            self.camera.hflip = True
+            self.camera.vflip = False
+            self.camera.brightness = 50
+            self.camera.preview_alpha = 120
+            self.camera.preview_fullscreen = True
 
-    def __enter__(self):
-        self.camera.__enter__()
-        return self
+        def __enter__(self):
+            self.camera.__enter__()
+            return self
 
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        self.camera.__exit__(exc_type, exc_val, exc_tb)
+        def __exit__(self, exc_type, exc_val, exc_tb):
+            self.camera.__exit__(exc_type, exc_val, exc_tb)
 
-    def take_picture(self, image_number: int):
-        time = datetime.now().strftime("%H_%M_%S")
-        filename = os.path.join(self.pictures_folder, time + "_" + str(image_number) + '.jpg')
-        self.camera.capture(filename)
-        return filename
+        def take_picture(self, image_number: int):
+            time = datetime.now().strftime("%H_%M_%S")
+            filename = os.path.join(self.pictures_folder, time + "_" + str(image_number) + '.jpg')
+            self.camera.capture(filename)
+            return filename
 
-    def with_preview(self, i, f):
-        self.camera.start_preview()
-        result = f(i)
-        self.camera.stop_preview()
-        return result
+        def with_preview(self, i, f):
+            self.camera.start_preview()
+            result = f(i)
+            self.camera.stop_preview()
+            return result
+
+except ImportError:
+
+    class Camera:
+        def __init__(self, pictures_folder):
+            self.pictures_folder = pictures_folder
+
+        def __enter__(self):
+            return self
+
+        def __exit__(self, exc_type, exc_val, exc_tb):
+            pass
+
+        def take_picture(self, image_number: int):
+            return "images/unicorns.jpg"
+
+        def with_preview(self, i, f):
+            return f(i)
