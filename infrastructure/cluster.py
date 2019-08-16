a49c75f22fa6ebc585c69104345d59e2841fe4ff
FaustXVI <xavier.detant@gmail.com>
2019-08-16 22:18:04 +0200

    Hide upload on background

--- infrastructure/cluster.py
+++ infrastructure/cluster.py
@@ -2,6 +2,8 @@ import requests
 import re
 import sys
 import traceback
+import threading
+import concurrent.futures
 
 url = "https://cluster.co/login/"
 upload_url = 'https://cluster-blobstoreimages.appspot.com/image_upload'
@@ -14,6 +16,26 @@ class Cluster:
         self.login = config['login']
         self.password = config['password']
         self.s = requests.Session()
+        self.executor = concurrent.futures.ThreadPoolExecutor(max_workers=4)
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.executor.__exit__(exc_type, exc_val, exc_tb)
+
+    def send(self, image):
+        files = {'upload': open(image, 'rb')}
+        res_upload = self.s.post(upload_url, files=files)
+        upload_id = str(res_upload.json()['uploadId'])
+        post = {
+            'url': 'photos',
+            'method': 'POST',
+            'headers': 'Content-Type=application%2Fx-www-form-urlencoded',
+            'body': 'cluster_id=' + self.album_id + '&gae_upload_id=' + upload_id,
+            'csrfToken': self.s.cookies['csrf_cluster']
+        }
+        self.s.post(post_url, post)
 
     def upload(self, images):
         try:
@@ -27,18 +49,7 @@ class Cluster:
                     'redirect': '/'
                 }
                 self.s.post(url, login, headers={'Referer': url})
-            for image in images:
-                files = {'upload': open(image, 'rb')}
-                res_upload = self.s.post(upload_url, files=files)
-                upload_id = str(res_upload.json()['uploadId'])
-                post = {
-                    'url': 'photos',
-                    'method': 'POST',
-                    'headers': 'Content-Type=application%2Fx-www-form-urlencoded',
-                    'body': 'cluster_id=' + self.album_id + '&gae_upload_id=' + upload_id,
-                    'csrfToken': self.s.cookies['csrf_cluster']
-                }
-                self.s.post(post_url, post)
+                self.executor.map(self.send, images)
             return True
         except:
             print("Cluster crash")
